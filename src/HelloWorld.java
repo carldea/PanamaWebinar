de0d833ee9dd37b9ab73912c8730838ea38d7850
Carl Dea <carldea@gmail.com>
2022-06-27 22:32:50 -0400

    Added code to demo printf's varidic arguments for method handles

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -28,6 +28,7 @@ public class HelloWorld {
 
             // invoke() requires a check exception
             printfMH.invoke(cString);
+            // Hello World! Panama style
 
             // requires casting return and arguments passed.
             //int charCount = (int) printfMH.invokeExact((Addressable) cString);
@@ -35,8 +36,29 @@ public class HelloWorld {
             // converting a C string (MemorySegment) into a Java String
             String jString = cString.getUtf8String(0);
             System.out.println("Convert to Java String: " + jString);
+            // Convert to Java String: Hello World! Panama style
 
-            // Hello World! Panama style
+            // Calling Variadic C functions such as printf("text %s %d", "text", 123);
+            System.out.println(Linker
+                    .downcallType(FunctionDescriptor.of(JAVA_INT) /* return type */
+                    .appendArgumentLayouts()
+                    .asVariadic(ADDRESS, ADDRESS, JAVA_INT))); /* arg1, arg2, arg3 */
+
+            var output = "Life, the %s and Everything is %d \n";
+
+            MethodHandle printfVaridicMH = linker.downcallHandle(
+                    symbol,
+                    FunctionDescriptor.of(JAVA_INT)
+                            .appendArgumentLayouts()
+                            .asVariadic(ADDRESS, ADDRESS, JAVA_INT));
+
+            Addressable arg1 = memorySession.allocateUtf8String(output);
+            Addressable arg2 = memorySession.allocateUtf8String("Universe");
+            int arg3 = 42;
+
+            printfVaridicMH.invoke(arg1, arg2, arg3);
+
+            // Life, the Universe and Everything is 42
        } catch (Throwable e) {
             throw new RuntimeException(e);
         }
